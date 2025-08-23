02cf4b6c0bd67a8efaaad4ff14e549afbec31760
Nicholas Zolton <nicholas@happyverse.ai>
2025-08-22 23:55:21 -0500

    add config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+from pydantic import computed_field
+import os
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    ZOTERO_ID: str
+    ZOTERO_KEY: str
+    ARXIV_QUERY: str
+    SMTP_SERVER: str
+    SMTP_PORT: int
+    SENDER: str
+    RECEIVER: str
+    SMTP_PASSWORD: str
+    SMTP_USERNAME: str = ""
+    OPENAI_API_KEY: str = ""
+    ZOTERO_IGNORE: str = ""
+    SEND_EMPTY: bool = False
+    MAX_PAPER_NUM: int = 100
+    USE_LLM_API: bool = False
+    OPENAI_API_BASE: str = "https://api.openai.com/v1"
+    MODEL_NAME: str = "gpt-5-nano"
+    LANGUAGE: str = "English"
+    DEBUG: bool = False
+
+
+settings = Config()  # type: ignore
