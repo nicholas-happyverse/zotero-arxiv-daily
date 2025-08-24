e3bebd4de4444a61877b9a38f9059979df07c815
Nicholas Zolton <nicholas@happyverse.ai>
2025-08-24 15:27:40 -0500

    generation of empty

--- generate_email.py
+++ generate_email.py
@@ -56,33 +56,26 @@ def filter_corpus(corpus: list[dict], pattern: str) -> list[dict]:
 def get_arxiv_paper(query: str, debug: bool = False) -> list[ArxivPaper]:
     client = arxiv.Client(num_retries=10, delay_seconds=10)
     feed = feedparser.parse(f"https://rss.arxiv.org/atom/{query}")
-    if "Feed error for query" in feed.feed.title:
+    if "Feed error for query" in feed.feed.title:  # type: ignore
         raise Exception(f"Invalid ARXIV_QUERY: {query}.")
-    if not debug:
-        papers = []
-        all_paper_ids = [
-            i.id.removeprefix("oai:arXiv.org:")
-            for i in feed.entries
-            if i.arxiv_announce_type == "new"
-        ]
-        bar = tqdm(total=len(all_paper_ids), desc="Retrieving Arxiv papers")
-        for i in range(0, len(all_paper_ids), 50):
-            search = arxiv.Search(id_list=all_paper_ids[i : i + 50])
-            batch = [ArxivPaper(p) for p in client.results(search)]
-            bar.update(len(batch))
-            papers.extend(batch)
-        bar.close()
 
-    else:
-        logger.debug("Retrieve 5 arxiv papers regardless of the date.")
-        search = arxiv.Search(
-            query="cat:cs.AI", sort_by=arxiv.SortCriterion.SubmittedDate
-        )
-        papers = []
-        for i in client.results(search):
-            papers.append(ArxivPaper(i))
-            if len(papers) == 5:
-                break
+    papers = []
+    all_paper_ids = []
+    for index, paper in enumerate(feed.entries):
+        if paper.arxiv_announce_type == "new":
+            all_paper_ids.append(paper.id.removeprefix("oai:arXiv.org:"))  # type: ignore
+        else:
+            logger.warning(
+                f"Skipping paper {index + 1}: {paper.title} for not being new."
+            )
+
+    bar = tqdm(total=len(all_paper_ids), desc="Retrieving Arxiv papers")
+    for i in range(0, len(all_paper_ids), 50):
+        search = arxiv.Search(id_list=all_paper_ids[i : i + 50])
+        batch = [ArxivPaper(p) for p in client.results(search)]
+        bar.update(len(batch))
+        papers.extend(batch)
+    bar.close()
 
     return papers
 
@@ -190,6 +183,7 @@ if __name__ == "__main__":
         corpus = filter_corpus(corpus, args.zotero_ignore)
         logger.info(f"Remaining {len(corpus)} papers after filtering.")
     logger.info("Retrieving Arxiv papers...")
+    logger.info("Retrieving Arxiv papers for {}...", args.arxiv_query)
     papers = get_arxiv_paper(args.arxiv_query, args.debug)
 
     if len(papers) == 0:
